f2d62b79bbc802045fc188cfe146d1ea6da4e5f7
dominik johann <dominikjohann@gmail.com>
2017-12-26 22:04:08 +0100

    car controller setup with very bad controls, post processing, standard cameras

---
+++ Assets/Cars/Scripts/SmoothFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothSpeed = 5.25f;
+	public Vector3 cameraOffset;
+
+	void FixedUpdate () {
+		Vector3 desiredPos = target.position + cameraOffset;
+		Vector3 smoothedPos = Vector3.Lerp (transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+		transform.position = smoothedPos;
+	}
+}
